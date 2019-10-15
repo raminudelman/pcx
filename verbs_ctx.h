e5f360fa69914334f6a9aadd33a31995188110e5
Rami Nudelman <rami.nudelman@gmail.com>
2019-10-15 09:27:29 +0300

    Remove mlx5dv import from verbs_ctx.h
    verbs_ctx.h does not use the mlx5dv API.

--- verbs_ctx.h
+++ verbs_ctx.h
@@ -35,9 +35,6 @@
 extern "C" {
 // Used for creating QPs/CQs.
 #include <infiniband/verbs.h>
-
-#include <cstring> // TODO: This include should be moved to mlx5dv. mlx5dv uses memcpy function without including propely the library!
-#include <infiniband/mlx5dv.h> // TODO: Check if this should be really included here. This should be included here only if verbs_ctx.* is actually using mlx5dv APIs!
 }
 
 // verbs_exp.h is included in order to:
