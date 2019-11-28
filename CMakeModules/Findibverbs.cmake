d0fe72241e79ab7e65c233ed592dae8d900ebcdb
Rami Nudelman <rami.nudelman@gmail.com>
2019-11-28 14:34:14 +0200

    Add CMake build files and sort files into directories

---
+++ CMakeModules/Findibverbs.cmake
@@ -0,0 +1,31 @@
+# Find the ibverbs libraries
+#
+# The following variables are optionally searched for defaults.
+# The user can set these variables in the ENV and during the compilation,
+# these variables will be searched.
+#  IBVERBS_ROOT_DIR: Base directory where all ibverbs components are found
+#  IBVERBS_INCLUDE_DIR: Directory where ibverbs headers are found
+#  IBVERBS_LIB_DIR: Directory where ibverbs libraries are found
+
+# The following are set after configuration is done:
+#  IBVERBS_FOUND
+#  IBVERBS_INCLUDE_DIRS
+#  IBVERBS_LIBRARIES
+
+find_path(IBVERBS_INCLUDE_DIRS
+  NAMES infiniband/verbs.h
+  HINTS
+  ${IBVERBS_INCLUDE_DIR}
+  ${IBVERBS_ROOT_DIR}
+  ${IBVERBS_ROOT_DIR}/include)
+
+find_library(IBVERBS_LIBRARIES
+  NAMES ibverbs
+  HINTS
+  ${IBVERBS_LIB_DIR}
+  ${IBVERBS_ROOT_DIR}
+  ${IBVERBS_ROOT_DIR}/lib)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ibverbs DEFAULT_MSG IBVERBS_INCLUDE_DIRS IBVERBS_LIBRARIES)
+mark_as_advanced(IBVERBS_INCLUDE_DIR IBVERBS_LIBRARIES)
